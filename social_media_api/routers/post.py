88144af534f1c09f2b117272ba88770f047731ce
Akhil Eaga <akhil.bitsian2013@gmail.com>
2023-10-26 15:48:04 +0530

    Added comments to posts

--- social_media_api/routers/post.py
+++ social_media_api/routers/post.py
@@ -1,13 +1,24 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
-from social_media_api.models.post import UserPost, UserPostIn
+from social_media_api.models.post import (
+    Comment,
+    CommentIn,
+    UserPost,
+    UserPostIn,
+    UserPostWithComments,
+)
 
 router = APIRouter()
 
 post_table = {}
+comment_table = {}
 
 
-@router.post("/posts", response_model=UserPost)
+def find_post(post_id):
+    return post_table.get(post_id)
+
+
+@router.post("/posts", response_model=UserPost, status_code=201)
 async def create_post(post: UserPostIn):
     data = post.model_dump()
     last_record_id = len(post_table)
@@ -19,3 +30,34 @@ async def create_post(post: UserPostIn):
 @router.get("/posts", response_model=list[UserPost])
 async def get_all_posts():
     return list(post_table.values())
+
+
+@router.post("/comments", response_model=Comment, status_code=201)
+async def create_comment(comment: CommentIn):
+    post = find_post(comment.post_id)
+    if not post:
+        raise HTTPException(status_code=404, detail="Post not found")
+    data = comment.model_dump()
+    last_record_id = len(comment_table)
+    new_comment = {**data, "id": last_record_id}
+    comment_table[last_record_id] = new_comment
+    return new_comment
+
+
+@router.get("/posts/{post_id}/comments", response_model=list[Comment])
+async def get_comments_on_post(post_id: int):
+    return [
+        comment for comment in comment_table.values() if comment["post_id"] == post_id
+    ]
+
+
+@router.get("/posts/{post_id}", response_model=UserPostWithComments)
+async def get_post_with_comments(post_id: int):
+    post = find_post(post_id)
+    if not post:
+        raise HTTPException(status_code=404, detail="Post not found")
+
+    return {
+        "post": post,
+        "comments": await get_comments_on_post(post_id),
+    }
